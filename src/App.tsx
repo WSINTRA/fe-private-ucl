aba015038cd0661faa3fd04ddd9ae5895b9c283c
WSINTRA <will.simpson85@gmail.com>
2022-02-28 23:56:20 -0500

    Created basic outline of routes and simple test for login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Preview } from "@mui/icons-material";
+import { Box, TextField } from "@mui/material";
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+export const App:React.FC =()=> {
 
-function App() {
+  const [authorized, setAuthorized] = React.useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Urgent Care Landscaping</h1>
+      <Routes>
+        {authorized ? 
+          (<><Route path="/" element={<Home />} /><Route path="about" element={<About />} /></>)
+        :
+        <>
+          <Route path="/" element={<Homey />} />
+        </> 
+        }
+      </Routes>
     </div>
   );
 }
 
-export default App;
+// App.js
+function Homey() {
+  const [loginForm, setLoginForm]= React.useState({
+    username: "",
+    password: ""
+  })
+   return (
+    <>
+      <Box>
+        <TextField onChange={(e)=>setLoginForm(prev => {return {...prev, username: e.target.value }} )} placeholder={'username'} />
+        <TextField type="password"
+          autoComplete="current-password" onChange={(e)=>setLoginForm(prev => {return {...prev, password: e.target.value }} )} placeholder={'password'} />
+      </Box>
+    </>
+  );
+}
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
